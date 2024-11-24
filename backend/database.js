d7dc8fd6605649b37d80158242fa34ad385c74da
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-11-24 12:55:48 +0100

    doc: update readme file

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,9 @@
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
-const username = process.env.MONGODB_USERNAME;
-const password = process.env.MONGODB_PASSWORD;
-
 // const uri = "mongodb://localhost:27017/productDB";
-const uri = `mongodb+srv://${username}:${password}@bonartisans.uhebi.mongodb.net/?retryWrites=true&w=majority&appName=bonartisans`;
+const uri = process.env.MONGODB_URI;
+
 const client = new MongoClient(uri, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
